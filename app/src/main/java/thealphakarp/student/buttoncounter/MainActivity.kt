6b3ef056b564b0761779af615d6cbf6dfd88e5b0
ArneSix <six.arne@gmail.com>
2020-04-15 23:26:37 +0200

    added basic button with listener

--- app/src/main/java/thealphakarp/student/buttoncounter/MainActivity.kt
+++ app/src/main/java/thealphakarp/student/buttoncounter/MainActivity.kt
@@ -2,11 +2,27 @@ package thealphakarp.student.buttoncounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var userName: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+    private var numClickCounter: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userName = findViewById<EditText>(R.id.editText)
+        button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+
+        button?.setOnClickListener {
+            numClickCounter += 1
+            textView?.append("\nclicked $numClickCounter times")
+        }
     }
 }
